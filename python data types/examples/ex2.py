0b8943b152b5412f34348da0e4f63d8106a5207e
Iliyas <iliasturlubek@gmail.com>
2024-01-25 00:11:05 +0600

    first

---
+++ python data types/examples/ex2.py
@@ -0,0 +1,55 @@
+x = str("Hello World")
+print(x)
+print(type(x))
+
+y = int(20)
+print(y)
+print(type(y))
+
+z = float(20.5)
+print(z)
+print(type(z))
+
+a = complex(1j)
+print(a)
+print(type(a))
+
+b = list(("apple", "banana", "cherry"))
+print(b)
+print(type(b))
+
+c = tuple(("apple", "banana", "cherry"))
+print(c)
+print(type(c))
+
+d = range(6)
+print(d)
+print(type(d))
+
+e = dict(name="John", age=36)
+print(e)
+print(type(e))
+
+f = set(("apple", "banana", "cherry"))
+print(f)
+print(type(f))
+
+g = frozenset(("apple", "banana", "cherry"))
+print(g)
+print(type(g))
+
+h = bool(5)
+print(h)
+print(type(h))
+
+i = bytes(5)
+print(i)
+print(type(i))
+
+j = bytearray(5)
+print(j)
+print(type(j))
+
+k = memoryview(bytes(5))
+print(k)
+print(type(k))
---
+++ python string/escape exa/ex1.py
@@ -0,0 +1,1 @@
+xt = "We are the so-called "Vikings" from the north."
